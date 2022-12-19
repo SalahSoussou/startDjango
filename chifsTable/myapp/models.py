02c10b5f668b2d07c4d4f620938d7d9be784840f
SalahSoussou <kaka79702@gmail.com>
2022-12-19 21:32:00 +0100

    ok

--- chifsTable/myapp/models.py
+++ chifsTable/myapp/models.py
@@ -4,7 +4,13 @@ from unicodedata import name
 # Create your models here.
 
 
+class Catigoty(models.Model):
+    menu_catigorys = models.CharField(max_length=200)
+
+
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
-    price = models.IntegerField()
+    count = models.IntegerField()
+    catigory_id = models.ForeignKey(
+        Catigoty, on_delete=models.PROTECT, default=None)
