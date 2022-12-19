c502d4066641e8222a5eac4454a02dee93967cd3
SalahSoussou <kaka79702@gmail.com>
2022-12-19 17:05:10 +0100

    ok

--- chifsTable/myapp/models.py
+++ chifsTable/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from unicodedata import name
 
 # Create your models here.
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    price = models.IntegerField()
